eeb25a88af32635efdf3445d9df437f5cc4bf1dd
Jesus <jesusjoanaazuara@gmail.com>
2026-05-09 18:39:27 +0200

    Test 2 Ok

---
+++ p5/src/server.py
@@ -0,0 +1,17 @@
+import socket
+
+from config import DEFAULT_HOST, DEFAULT_PORT
+
+
+class FileServer:
+    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
+        self.host = host
+        self.port = port
+
+    def create_socket(self):
+        return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def build_response(self, file_path):
+        # El servidor responde con el contenido textual del fichero solicitado.
+        with open(file_path, "r", encoding="utf-8") as file:
+            return file.read()
