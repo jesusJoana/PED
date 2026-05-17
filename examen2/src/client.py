7c2517f50c56c3e9bc49f13fb396288e91bf609d
Jesus <jesusjoanaazuara@gmail.com>
2026-05-17 13:03:31 +0200

    Refactor 2 - Cliente

--- examen2/src/client.py
+++ examen2/src/client.py
@@ -20,14 +20,32 @@ class UDPInfoClient:
         if output is None:
             output = sys.stdout
 
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            sock.settimeout(self.timeout)
-
+        with self._create_socket() as sock:
             for message in self.messages:
-                try:
-                    sock.sendto(message.encode("utf-8"), (self.host, self.port))
-                    data, _address = sock.recvfrom(BUFFER_SIZE)
-                    print(data.decode("utf-8"), file=output)
-                except OSError:
-                    print("ERROR", file=output)
+                response = self._send_and_receive(sock, message)
+                self._print_response(output, response)
+                if response is None:
                     break
+
+    def _create_socket(self):
+        """Crea el socket UDP configurado con timeout."""
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(self.timeout)
+        return sock
+
+    def _send_and_receive(self, sock, message):
+        """Envia un mensaje UDP y devuelve la respuesta recibida."""
+        try:
+            sock.sendto(message.encode("utf-8"), (self.host, self.port))
+            data, _address = sock.recvfrom(BUFFER_SIZE)
+            return data.decode("utf-8")
+        except OSError:
+            return None
+
+    def _print_response(self, output, response):
+        """Imprime la respuesta recibida o ERROR si no hubo respuesta."""
+        if response is None:
+            print("ERROR", file=output)
+            return
+
+        print(response, file=output)
